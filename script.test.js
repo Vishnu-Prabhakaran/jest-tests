1945422acbd5c30db9283995b9b48f6279382348
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-26 22:36:07 +1100

    Grouping tests

--- script.test.js
+++ script.test.js
@@ -3,28 +3,31 @@ const googleSearch = require('./script');
 // Database mock
 dbMock = ['dog.com', 'cheespuff.com', 'disney.com', 'dogpictures'];
 
-it('this is a test', () => {
-  // console.log(googleSearch)
-  // googleSearch('testtest', dbMock)
-  expect('hello').toBe('hello');
-});
+// Grouping
+describe('googleSearch', () => {
+  it('this is a test', () => {
+    // console.log(googleSearch)
+    // googleSearch('testtest', dbMock)
+    expect('hello').toBe('hello');
+  });
 
-// passed
-it('Is searching google', () => {
-  // console.log(googleSearch)
-  // googleSearch('testtest', dbMock)
-  expect(googleSearch('testtest', dbMock)).toEqual([]);
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures']);
-});
+  // passed
+  it('Is searching google', () => {
+    // console.log(googleSearch)
+    // googleSearch('testtest', dbMock)
+    expect(googleSearch('testtest', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures']);
+  });
 
-it ('work with undefined and null input' , () => {
+  it('work with undefined and null input', () => {
     // If we get an undefined input from the user we want to return an empty array
     expect(googleSearch('undefined', dbMock)).toEqual([]);
     expect(googleSearch('null', dbMock)).toEqual([]);
-})
+  });
 
-it ('it does not return more than 3 matches' , () => {
+  it('it does not return more than 3 matches', () => {
     // If we get 3 results back
     expect(googleSearch('.com', dbMock).length).toEqual(3);
     //expect(googleSearch('null', dbMock)).toEqual([]);
-})
+  });
+});
