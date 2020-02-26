991ccf30ae121642f9819e90d02c2d0a3f125902
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-26 22:34:05 +1100

    more tests

--- script.test.js
+++ script.test.js
@@ -16,3 +16,15 @@ it('Is searching google', () => {
   expect(googleSearch('testtest', dbMock)).toEqual([]);
   expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures']);
 });
+
+it ('work with undefined and null input' , () => {
+    // If we get an undefined input from the user we want to return an empty array
+    expect(googleSearch('undefined', dbMock)).toEqual([]);
+    expect(googleSearch('null', dbMock)).toEqual([]);
+})
+
+it ('it does not return more than 3 matches' , () => {
+    // If we get 3 results back
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+    //expect(googleSearch('null', dbMock)).toEqual([]);
+})
